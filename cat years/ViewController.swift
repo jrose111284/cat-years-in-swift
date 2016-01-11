8efffc15a31d1a1b019cf243dcb89f93f8889f1c
jamierose <jrose111284@gmail.com>
2016-01-11 21:30:00 +0000

    completed cat years

--- cat years/ViewController.swift
+++ cat years/ViewController.swift
@@ -10,6 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var age: UITextField!
+    
+    @IBAction func findAge(sender: AnyObject) {
+        
+      let enterAge = Int(age.text!)
+        
+        if enterAge != nil {
+            let catyears = enterAge! * 7
+            resultLabel.text = "your cat is \(catyears) in cat year"
+        } else {
+            resultLabel.text = "please enter a whole number"
+        }
+    }
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
